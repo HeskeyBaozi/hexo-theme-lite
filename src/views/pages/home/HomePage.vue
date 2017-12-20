de6b59f75ed340eb420f78f512ff3134ed1c53eb
HeskeyBaozi <hezhiyu233@foxmail.com>
2017-12-20 18:32:52 +0800

    给Home页面back按钮增加下margin防止与标题黏在一起

--- src/views/pages/home/HomePage.vue
+++ src/views/pages/home/HomePage.vue
@@ -1,48 +1,54 @@
 <template>
   <div id="home-page">
-    <el-button v-show="page !== 1" plain size="mini" @click="onPage(1)">
+    <el-button class="back" v-show="page !== 1" plain size="mini" @click="onPage(1)">
       <i class="fa fa-undo" aria-hidden="true"></i>
       <span>Back</span>
     </el-button>
     <div class="article-card-list">
       <article-card v-for="post of posts"
-                  :key="post.slug"
-                  :format="format"
-                  :post="post"
-                  :showPhotos="true"
-                  @photo-zoom-in="showPhotoDetail"></article-card>
+                    :key="post.slug"
+                    :format="format"
+                    :post="post"
+                    :showPhotos="true"
+                    @photo-zoom-in="showPhotoDetail"></article-card>
     </div>
     <el-pagination class="pagination" v-if="shouldPage" small layout="prev, pager, next"
-                  :total="pagination.total"
-                  :page-size="pagination.pageSize"
-                  :current-page="page" @current-change="onPage"></el-pagination>
+                   :total="pagination.total"
+                   :page-size="pagination.pageSize"
+                   :current-page="page" @current-change="onPage"></el-pagination>
     <el-dialog :visible.sync="modal.isShown" width="min-content">
-      <img class="modal-picture" :src="modal.url" :alt="modal.url" @click="modal.isShown = false" />
+      <img class="modal-picture" :src="modal.url" :alt="modal.url" @click="modal.isShown = false"/>
     </el-dialog>
   </div>
 </template>
 
 <style lang="less" scoped>
-.modal-picture {
-  max-width: 950px;
-  min-width: 150px;
-  min-height: 100px;
-  cursor: zoom-out;
-}
+  #home-page {
+    .back {
+      margin-bottom: 1rem;
+    }
 
-.pagination {
-  text-align: center;
-}
+    .modal-picture {
+      max-width: 950px;
+      min-width: 150px;
+      min-height: 100px;
+      cursor: zoom-out;
+    }
+
+    .pagination {
+      text-align: center;
+    }
+  }
 </style>
 
 <style lang="less">
-.el-pagination .el-pager .active {
-  background-color: #5c5c5c;
-  color: white;
-  border-radius: 0.2rem;
-  margin-left: 0.2rem;
-  margin-right: 0.2rem;
-}
+  .el-pagination .el-pager .active {
+    background-color: #5c5c5c;
+    color: white;
+    border-radius: 0.2rem;
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+  }
 </style>
 
 
